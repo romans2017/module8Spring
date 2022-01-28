2fb13cc678e6b4f5c9cd855f721c897579b3537c
romans2017 <savchuk.r0@gmail.com>
2022-01-28 14:40:46 +0200

    add demo populate

--- src/main/resources/db/migration/V1.1.0__Populate.sql
+++ src/main/resources/db/migration/V1.1.0__Populate.sql
@@ -2,5 +2,7 @@ INSERT INTO roles (id, name) VALUES ('00000000-0000-0000-0000-000000000001', 'RO
 INSERT INTO roles (id, name) VALUES ('00000000-0000-0000-0000-000000000002', 'ROLE_USER');
 --P@ssword?654
 INSERT INTO users (id, email, first_name, password) VALUES ('00000000-0000-0000-0000-000000000003', 'admin@admin.org', 'admin', '{bcrypt}$2a$12$mJH7EcR0mLbmV6Zzf7tbX.nsTcWcELPxiPoFMwhNFC5slwJ4jP35G');
-
 INSERT INTO users_roles (role_id, user_id) VALUES ('00000000-0000-0000-0000-000000000001', '00000000-0000-0000-0000-000000000003');
+--user
+INSERT INTO users (id, email, first_name, password) VALUES ('00000000-0000-0000-0000-000000000004', 'user@user.org', 'user', '{bcrypt}$2y$10$nmqdWjbaj6ZMbSn8SdZEGuwIna0zJqjcvPMqDmtlneMat4pVZYGaa');
+INSERT INTO users_roles (role_id, user_id) VALUES ('00000000-0000-0000-0000-000000000002', '00000000-0000-0000-0000-000000000004');
